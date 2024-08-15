d919ec18f5737bfc7ee8de940715d69804936acc
SirPanharith <ssak@paragoniu.edu.kh>
2024-08-15 15:56:41 +0700

    Edit base URL of team invitation

--- app/Http/Controllers/TeamInvitationController.php
+++ app/Http/Controllers/TeamInvitationController.php
@@ -82,7 +82,7 @@ class TeamInvitationController extends Controller
     ]);
 
     // Manually set the base URL to 0.0.0.0:8000
-    $baseUrl = 'http://0.0.0.0:8000';
+    $baseUrl = 'http://143.198.209.104';
     // Generate the unique link
     $link = $baseUrl . '/accept-invitation/' . $invitationToken;
 
